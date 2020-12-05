6efa48f41c38f215735457a58898a4d383ef7aa2
“Varunps91” <Starklabsindia@gmail.com>
2020-12-05 20:08:51 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
 function App() {
+  
+  const[inputText, setInputText] = useState("");
+  const[todos, setTodos] = useState([]);
+  const[status, setStatus] = useState("all");
+  const[filteredTodos, setFilteredTodos] = useState([]);
+
+  // function 
+
+  useEffect(()=>{
+    filterHandler();
+    saveLocal();
+  },[todos, status]);
+
+  const filterHandler = ()=>{
+    switch(status){
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true));
+        break;
+      
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+
+    }
+  }
+
+  const saveLocal =()=>{
+    if(localStorage.getItem('todos') === null){
+      localStorage.setItem('todos', JSON.stringify([]));
+    }else{
+      localStorage.setItem('todos', JSON.stringify(todos));
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Varun Todo List</h1>
       </header>
+      <Form todos={todos} setTodos={setTodos} setInputText = {setInputText} inputText={inputText} setStatus={setStatus} />
+      <TodoList todos={todos} setTodos={setTodos} filteredTodos={filteredTodos} />
     </div>
   );
 }
